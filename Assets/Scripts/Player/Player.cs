0892fcbfaa35ca0d24ef6d248da95d06f1bcb003
Daniel Giani <dnsgiani@gmail.com>
2022-07-17 23:20:32 -0300

    player animations
    player animation on jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
 
-    public Rigidbody2D myRigidbody;
+    [Header("Speed Setup")]
     public Vector2 friction = new Vector2(-.1f, 0);
     public float speed;
-    public float forceJump = 2;
-
     public float speedRun;
+    public float forceJump = 2;
+    public Rigidbody2D myRigidbody;
     private float _currentSpeed;
+
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.7f;
+    public float animationDuration = .3f;
+
     private void Update() 
     {
         HandleJump();
@@ -51,5 +58,15 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
             myRigidbody.velocity = Vector2.up * forceJump;
+            myRigidbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidbody.transform);
+            HandleScaleJump();
+    }
+
+    private void HandleScaleJump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo);
     }
 }
