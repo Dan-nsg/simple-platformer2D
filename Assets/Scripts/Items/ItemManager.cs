42e6078743179dcb3afc9e7cce822be3e8e77b6e
Daniel Giani <dnsgiani@gmail.com>
2022-07-31 11:52:10 -0300

    update item manager

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -1,20 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Plataformer2D.Core.Singleton;
 
-public class ItemManager : MonoBehaviour
+public class ItemManager : Singleton<ItemManager>
 {
-
-    public static ItemManager Instance;
     public int coins;
-
-    private void Awake() 
-    {
-        if(Instance == null)
-            Instance = this;
-        else
-            Destroy(gameObject);
-    }
 
     private void Start() 
     {
